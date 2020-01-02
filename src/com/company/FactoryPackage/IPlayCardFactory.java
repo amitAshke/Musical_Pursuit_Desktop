8e95be4136abd5b516d904659a4dfc16b396fb28
Amit Ashkenazy <amit471@gmail.com>
2020-01-02 14:39:36 +0200

    Added the factory package.

---
+++ src/com/company/FactoryPackage/IPlayCardFactory.java
@@ -0,0 +1,8 @@
+package com.example.dbcourse.Musical_Pursuit.backend.FactoryPackage;
+
+import com.example.dbcourse.Musical_Pursuit.backend.PlayCardPackage.IPlayCard;
+
+public interface IPlayCardFactory {
+
+    public IPlayCard CreatePlayCard();
+}
