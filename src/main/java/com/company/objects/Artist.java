96e306d3da1c6a04995e2ca1347784bf8a5f02c9
Amit Ashkenazy <amit471@gmail.com>
2020-01-28 12:28:13 +0200

    Remade the objects of interest.

---
+++ src/main/java/com/company/objects/Artist.java
@@ -0,0 +1,19 @@
+package com.company.objects;
+
+public class Artist {
+    private String artistID;
+    private String artistName;
+
+    public Artist(String artistID, String artistName) {
+        this.artistID = artistID;
+        this.artistName = artistName;
+    }
+
+    public String getArtistID() {
+        return artistID;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+}
---
+++ src/main/java/com/company/objects/Song.java
@@ -0,0 +1,32 @@
+package com.company.objects;
+
+public class Song {
+    private String songID;
+    private String title;
+    private int year;
+    private Artist artist;
+
+    public Song(String songID, String title, int year, Artist artist) {
+        this.songID = songID;
+        this.title = title;
+        this.year = year;
+        this.artist = artist;
+    }
+
+    public String getSongID() {
+        return songID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public Artist getArtist() {
+        return artist;
+    }
+
+}
